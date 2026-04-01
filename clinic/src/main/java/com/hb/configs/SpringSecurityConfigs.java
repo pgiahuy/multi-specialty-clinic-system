267eb2fcac587a6f90e209aa0b7188a4e597585b
GiaHuy999 <huyphan2792005@gmail.com>
2026-04-01 11:36:51 +0700

    feat: base configs + entity POJOs

--- clinic/src/main/java/com/hb/configs/SpringSecurityConfigs.java
+++ clinic/src/main/java/com/hb/configs/SpringSecurityConfigs.java
@@ -4,10 +4,72 @@
  */
 package com.hb.configs;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
 /**
  *
  * @author HUY
  */
+@Configuration
+@EnableWebSecurity
+@EnableTransactionManagement
+@ComponentScan(
+        basePackages = {
+            "com.hb.controllers",
+            "com.hb.repository",
+            "com.hb.service"
+        }
+)
 public class SpringSecurityConfigs {
-    
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+        return new HandlerMappingIntrospector();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(c -> c.disable()).authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/", "/admin").hasRole("ADMIN")
+                .requestMatchers("/api/**").permitAll()
+                .anyRequest().authenticated()
+        ).formLogin(form -> form.loginPage("/admin/login")
+                .loginProcessingUrl("/login") 
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/admin/login?error=true")
+                .permitAll()
+        ).logout((logout) -> logout.logoutSuccessUrl("/admin/login").permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Cloudinary cloudinary
+                = new Cloudinary(ObjectUtils.asMap(
+                        "cloud_name", "dyojjvkxz",
+                        "api_key", "YOUR_CLOUDINARY_API_KEY",
+                        "api_secret", "YOUR_CLOUDINARY_API_SECRET",
+                        "secure", true));
+        return cloudinary;
+    }
 }
--- clinic/src/main/java/com/hb/configs/WebAppContextConfigs.java
+++ clinic/src/main/java/com/hb/configs/WebAppContextConfigs.java
@@ -4,10 +4,52 @@
  */
 package com.hb.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  *
  * @author baon6
  */
-public class WebAppContextConfigs {
+@Configuration
+@ComponentScan(
+        basePackages = {
+            "com.hb.controllers",
+            "com.hb.repository",
+            "com.hb.service"
+        }
+)
+@EnableWebMvc
+@EnableTransactionManagement
+public class WebAppContextConfigs implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+//        
+    }
+
+    @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+    }
+
     
 }
