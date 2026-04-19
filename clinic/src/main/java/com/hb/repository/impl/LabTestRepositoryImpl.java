7c689c85cb4ecfc2a3cca6b7564af571a62de0d0
Bao Nguyen <baondq205@gmail.com>
2026-04-19 16:41:12 +0700

    add area, room, test controller

---
+++ clinic/src/main/java/com/hb/repository/impl/LabTestRepositoryImpl.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hb.repository.impl;
+
+import com.hb.pojo.LabTests;
+import com.hb.repository.LabTestRepository;
+import java.util.List;
+import java.util.Map;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author DELL
+ */
+@Repository
+@Transactional
+@PropertySource("classpath:configs.properties")
+public class LabTestRepositoryImpl implements LabTestRepository{
+    @Autowired
+    private LocalSessionFactoryBean factory;
+    
+    @Autowired
+    private Environment env;
+
+    @Override
+    public List<LabTests> getLabTests(Map<String, String> params) {
+        Session session = this.factory.getObject().getCurrentSession();
+        Query<LabTests> q = session.createNamedQuery("LabTests.findAll", LabTests.class);
+
+        if (params != null && params.containsKey("page")) {
+            int pageSize = Integer.parseInt(env.getProperty("labtests.page_size", "10"));
+            int page = Integer.parseInt(params.getOrDefault("page", "1"));
+            q.setMaxResults(pageSize);
+            q.setFirstResult((page - 1) * pageSize);
+        }
+        return q.getResultList();
+    }
+
+    @Override
+    public LabTests getLabTestById(Integer id) {
+        Session session = this.factory.getObject().getCurrentSession();
+        return session.get(LabTests.class, id);
+    }
+
+    @Override
+    public void addOrUpdateLabTest(LabTests test) {
+        Session session = this.factory.getObject().getCurrentSession();
+        if (test.getId() != null) {
+            session.merge(test);
+        } else {
+            session.persist(test);
+        }
+    }
+
+    @Override
+    public void deleteLabTest(Integer id) {
+        Session session = this.factory.getObject().getCurrentSession();
+        LabTests test = this.getLabTestById(id);
+        if (test != null) {
+            session.remove(test);
+        }
+    }
+}
---
+++ clinic/src/main/java/com/hb/service/impl/AreasServiceImpl.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hb.service.impl;
+
+import com.hb.pojo.Areas;
+import com.hb.repository.AreasRepository;
+import com.hb.service.AreasService;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author DELL
+ */
+@Service
+public class AreasServiceImpl implements AreasService {
+
+    @Autowired
+    private AreasRepository areaRepo;
+
+    @Override
+    public List<Areas> getAreas(Map<String, String> params) {
+        return areaRepo.getAreas(params);
+    }
+
+    @Override
+    public Areas getAreasById(Long id) {
+        Areas a = areaRepo.getAreasById(id);
+        if (a == null) {
+            throw new RuntimeException("Doctor not found!");
+        }
+        return a;
+    }
+
+    @Override
+    public Areas addArea(Map<String, String> params) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deleteAreas(Long id) {
+        this.areaRepo.deleteAreas(id);
+    }
+
+}
