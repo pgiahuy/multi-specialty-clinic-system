1342d7c12b80f7362c385d38152962efe264a5e3
GiaHuy999 <huyphan2792005@gmail.com>
2026-05-08 17:03:10 +0700

    feat(prescription): base CRUD

--- clinic/src/main/java/com/hb/mapper/PrescriptionMapper.java
+++ clinic/src/main/java/com/hb/mapper/PrescriptionMapper.java
@@ -4,10 +4,26 @@
  */
 package com.hb.mapper;
 
+import com.hb.dto.response.PrescriptionResponse;
+import com.hb.pojo.Prescription;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
 /**
  *
  * @author HUY
  */
+
+@Mapper(uses = PrescriptionItemMapper.class)
 public interface PrescriptionMapper {
+    
+    PrescriptionMapper INSTANCE = Mappers.getMapper(PrescriptionMapper.class);
+
+    @Mapping(source = "medicalRecordId.appointmentId.scheduleId.doctorId.fullName", target = "doctorName")
+    @Mapping(source = "medicalRecordId.appointmentId.patientId.fullName", target = "patientName")
+    @Mapping(source = "prescriptionItemCollection", target = "items")
+    @Mapping(source = "medicalRecordId.note", target = "note")
+    PrescriptionResponse toResponse(Prescription prescription);
     
 }
