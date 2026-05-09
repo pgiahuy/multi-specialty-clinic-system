5e9ffb739e880923bb76d631443fa49b8b977d42
Bao Nguyen <baondq205@gmail.com>
2026-05-09 16:48:09 +0700

    feat: doctor API + appointment refactor (#54)

--- clinic/src/main/java/com/hb/service/AppointmentService.java
+++ clinic/src/main/java/com/hb/service/AppointmentService.java
@@ -7,7 +7,6 @@ package com.hb.service;
 import com.hb.dto.request.AppointmentCreateRequest;
 import com.hb.dto.response.AppointmentResponse;
 import com.hb.pojo.Appointment;
-import com.hb.repository.AppointmentRepository;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +17,6 @@ import java.util.Map;
 public interface AppointmentService {
     List<Appointment> getAppointments(Map<String,String> params);
     Appointment getAppointmentById(Long id);
-    AppointmentResponse addAppointment(AppointmentCreateRequest req);
+    AppointmentResponse addOrUpdateAppointment(AppointmentCreateRequest req);
     long countAppointments(Map<String,String> params);
 }
---
+++ clinic/src/main/java/com/hb/service/LabTestResultService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.hb.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author DELL
+ */
+@Service
+public interface LabTestResultService {
+    
+}
