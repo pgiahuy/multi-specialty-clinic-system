a8de2415830a7acbfdfd6946598042ea4da73c2e
GiaHuy999 <huyphan2792005@gmail.com>
2026-05-25 20:09:51 +0700

    fix: push notifications (#68)

--- clinic/src/main/java/com/hb/configs/FirebaseConfig.java
+++ clinic/src/main/java/com/hb/configs/FirebaseConfig.java
@@ -4,13 +4,6 @@
  */
 package com.hb.configs;
 
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import jakarta.annotation.PostConstruct;
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
