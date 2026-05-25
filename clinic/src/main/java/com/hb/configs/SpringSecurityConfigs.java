1f179d0e484777f3a787054e391747b99273aa7c
GiaHuy999 <huyphan2792005@gmail.com>
2026-05-26 01:23:00 +0700

    fix: Spring Security config (#90)

--- clinic/src/main/java/com/hb/configs/SpringSecurityConfigs.java
+++ clinic/src/main/java/com/hb/configs/SpringSecurityConfigs.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -46,6 +47,7 @@ public class SpringSecurityConfigs {
     private UserDetailsService userDetailsService;
 
     @Bean
+    @Lazy
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
